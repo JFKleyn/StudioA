91a7a249b2552e29612e5f88c4ea237c84046ce0
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-01-06 20:39:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 document.getElementById("contact-btn").addEventListener("click", function () {
     window.location.href = "contact.html";
   });
+
+const cards = document.querySelectorAll('.card');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+      } else {
+        entry.target.classList.remove('in-view'); // optional
+      }
+    });
+  },
+  {
+    threshold: 0.4
+  }
+);
+
+cards.forEach(card => observer.observe(card));
