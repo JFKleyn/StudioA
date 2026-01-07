e54f705231213020aada2e595b79a5af7e30fbbd
JFKleyn <101085158+JFKleyn@users.noreply.github.com>
2026-01-07 18:37:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,13 +20,6 @@ const observer = new IntersectionObserver(
   }
 );
 
-cards.forEach((card) => {
-  observer.observe(card);
-  
-  // Force hardware acceleration for iOS Safari
-  card.style.transform = 'translateZ(0)';
-  card.style.backfaceVisibility = 'hidden';
-});
 
 cards.forEach((card) => {
   observer.observe(card);
